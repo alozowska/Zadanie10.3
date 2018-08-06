f67bf50cc9be3ac560c3d1a1227a957f04addbdb
Agnieszka Łozowska <aga.lozowska@wp.pl>
2018-08-06 16:52:26 +0200

    Poprawiona petla i metody

--- src/TestProdukty.java
+++ src/TestProdukty.java
@@ -7,20 +7,36 @@ public class TestProdukty {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("produkty.csv");
         Scanner scan = new Scanner(file);
+        Product[] products = new Product[3];
+        int i=0;
         while (scan.hasNextLine()) {
             String product = scan.nextLine();
             String[] tablica = product.split(";");
-            Product p = new Product(tablica[0], tablica[1], Double.valueOf(tablica[2]));
-            Product[] products = new Product[tablica.length];
-            public double obliczSume (Product[]products){
-                double suma = 0;
-                for (int i = 0; i < products.length; i++) {
-                    double suma = suma + product[i].getCena;
-                }
-                return suma;
-            }
-
-
+            products[i] = new Product(tablica[0], tablica[1], Double.valueOf(tablica[2]));
+            i++;
         }
+        double wynikSumowania=obliczSume( products );
+        System.out.println(wynikSumowania);
+        Product najdrozszy=znajdzNajdrozszy(products);
+        System.out.println(najdrozszy);
+
     }
+    private static double obliczSume (Product[] products){
+        double suma = 0;
+        for (int i = 0; i < products.length; i++) {
+            suma = suma + products[i].getCena();
+        }
+        return suma;
+
+    }
+    private static Product znajdzNajdrozszy(Product[] products){
+         Product max=products[0];
+        for (int i = 1; i < products.length; i++) {
+            if (products[i].getCena()>max.getCena()){
+                max=products[i];
+            }
+        }
+        return max;
+    }
+
 }
